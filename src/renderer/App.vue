372e9d4d4c5abf83d732adc95dc276813b93f85c
hcb79 <hcb@hcb.de>
2018-07-02 17:00:45 +0200

    Navigation hinzugefügt.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <b-navbar toggleable type="light" variant="light">
+        <div class="container">
+          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+          <b-navbar-brand>
+            <router-link to="/" class="navbar-brand"><img alt="WBÖ" title="WBÖ" width="100" height="55" src="~@/assets//wboelogo100.png"> Redaktionstool</router-link>
+           </b-navbar-brand>
+          <b-collapse is-nav id="nav_collapse">
+              <b-navbar-nav class="ml-auto">
+                <b-nav-item to="/">Home</b-nav-item>
+              </b-navbar-nav>
+          </b-collapse>
+        </div>
+    </b-navbar>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -16,4 +31,17 @@
 <style>
 	/* @import url('~@/assets/css/bootstrap.min.css'); */
   /* CSS */
+  .mit0 {
+    margin-top: 0px;
+  }
+  .mil5 {
+    margin-left: 5px;
+  }
+  .navbar-brand > img {
+    width: 100px;
+    margin-top: -13px;
+    float: left;
+    margin-right: 20px;
+    margin-bottom: -13px;
+  }
 </style>
