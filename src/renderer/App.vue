3b4f7e6b07f170f07a8af8617c99400015a1209d
hcb79 <hcb@hcb.de>
2018-07-02 16:40:17 +0200

    "bootstrap-vue" hinzugefügt.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,11 +5,15 @@
 </template>
 
 <script>
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
   export default {
     name: 'redaktionstool-electron-vue'
   }
 </script>
 
 <style>
+	/* @import url('~@/assets/css/bootstrap.min.css'); */
   /* CSS */
 </style>
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -10,20 +10,7 @@
       </div>
 
       <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
+        <b-alert show>Default Alert</b-alert>
       </div>
     </main>
   </div>
@@ -44,7 +31,7 @@
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  /* @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro'); */
 
   * {
     box-sizing: border-box;
