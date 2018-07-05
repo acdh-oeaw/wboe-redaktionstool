076b2366eaae73c1383c2aec497810f732cc5b74
hcb79 <hcb@hcb.de>
2018-07-05 15:22:27 +0200

    Verzeichniss auslesen und anzeigen.

--- src/renderer/components/StartPage.vue
+++ src/renderer/components/StartPage.vue
@@ -1,16 +1,51 @@
 <template>
 	<div class="start-page">
 		<h1>Übersicht</h1>
+		<FileLine :file="file" v-for="(file, fKey) in files" :key="fKey"/>
 	</div>
 </template>
 
 <script>
+import FileLine from './StartPage/FileLine'
+import { remote } from 'electron'
+import path from 'path'
+const fs = remote.require('fs')
+
 export default {
 	name: 'start-page',
 	data () {
 		return {
-			'test': 'Test'
+			folder: undefined,
+			files: []
 		}
+	},
+	watch: {
+		folder: function (nVal) {
+			if (nVal !== undefined) {
+				this.files = this.readFolder(this.folder)
+			}
+		}
+	},
+	methods: {
+		readFolder (aPath) {
+			var aPathContent = []
+			fs.readdirSync(aPath).forEach(function (file) {
+				var aFullFileName = path.join(aPath, file)
+				var stats = fs.statSync(aFullFileName)
+				var aFileData = {file: file, fullFileName: aFullFileName, isDir: stats.isDirectory(), size: stats.size}
+				if (aFileData.isDir) {
+					aFileData.folderContent = this.readFolder(aFullFileName)
+				}
+				aPathContent.push(aFileData)
+			}, this)
+			return aPathContent
+		}
+	},
+	mounted: function () {
+		this.folder = remote.app.getPath('userData')
+	},
+	components: {
+		FileLine
 	}
 }
 </script>
---
+++ src/renderer/components/StartPage/FileLine.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="file">
+		<div class="fileline">
+			<button @click="isOpen = !isOpen"><font-awesome-icon :icon="((file.isDir) ? ((isOpen) ? 'folder-open' : 'folder') : 'file')"/>
+				{{ file.file }}
+				<span class="filesize" v-if="!file.isDir">{{ file.size }}</span>
+			</button>
+		</div>
+		<FileLine :file="fFile" v-for="(fFile, fKey) in file.folderContent" :key="fKey" v-if="file.isDir && isOpen"/>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'FileLine',
+		props: {
+			file: Object
+		},
+		data () {
+			return {
+				'isOpen': false
+			}
+		},
+		computed: {
+		}
+	}
+</script>
+
+<style scoped>
+	.fileline > button {
+		width: 100%;
+		text-align: left;
+		margin: 0px;
+		padding: 0px;
+		background: none;
+		border: none;
+	}
+	.file > .file {
+    margin-left: 25px;
+	}
+	.filesize {
+		float: right;
+	}
+</style>
