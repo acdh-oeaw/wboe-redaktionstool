c3c686d607388f425d4914b9de0e1c442715491e
hcb79 <hcb@hcb.de>
2018-07-06 11:20:42 +0200

    ViewXmlProEditor - Funktionen teilweise ausgelagert.

--- src/renderer/components/ToolPage/ViewXmlProEditor.vue
+++ src/renderer/components/ToolPage/ViewXmlProEditor.vue
@@ -7,22 +7,7 @@
 </template>
 
 <script>
-	import 'codemirror/mode/javascript/javascript.js'
-	import 'codemirror/mode/gfm/gfm.js'
-	import 'codemirror/mode/markdown/markdown.js'
-	import 'codemirror/mode/xml/xml.js'
-	import 'codemirror/addon/edit/closetag.js'
-	import 'codemirror/addon/edit/continuelist.js'
-	import 'codemirror/addon/edit/closebrackets.js'
-	import 'codemirror/addon/lint/lint.js'
-	import 'codemirror/addon/mode/overlay.js'
-	import 'codemirror/addon/fold/foldcode.js'
-	import 'codemirror/addon/fold/foldgutter.js'
-	import 'codemirror/addon/fold/brace-fold.js'
-	import 'codemirror/addon/fold/xml-fold.js'
-	import 'codemirror/addon/fold/markdown-fold.js'
-	import 'codemirror/addon/fold/comment-fold.js'
-	import 'codemirror/addon/selection/active-line.js'
+	import ViewXmlProEditorFunctions from './ViewXmlProEditorFunctions'
 
 	export default {
 		name: 'ViewXmlProEditor',
@@ -32,26 +17,7 @@
 		data () {
 			return {
 				code: '',
-				cmOptions: {
-					mode: 'text/html',
-					theme: 'default',
-					autoCloseTags: true,
-					showCursorWhenSelecting: true,
-					lineNumbers: true,
-					lineWrapping: true,
-					tabSize: 4,
-					foldGutter: true,
-					electricChars: true,
-					styleActiveLine: true,
-					matchBrackets: true,
-					dragDrop: false,
-					autoCloseBrackets: true,
-					autoRefresh: true,
-					styleSelectedText: true,
-					line: true,
-					indentWithTabs: true,
-					gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-				},
+				cmOptions: ViewXmlProEditorFunctions.cmOptions,
 				refreshCodemirror: true,
 				codemirrorReady: false
 			}
@@ -60,35 +26,19 @@
 			code: function (nVal, oVal) {
 				// console.log('changed')
 			},
-			refreshCodemirror: function (nVal) {
-				if (nVal) {
-					this.refreshCM()
-				}
-			}
+			refreshCodemirror: ViewXmlProEditorFunctions.refreshCodemirror,
 		},
 		mounted () {
 			this.code = this.xmlString
 		},
 		methods: {
-			refresh () {
-				this.refreshCodemirror = true
-			},
-			refreshCM () {
-				if (this.codemirrorReady) {
-					this.$refs.myCm.refresh()
-					this.refreshCodemirror = false
-				}
-			},
-			onCmReady (cm) {
-				this.codemirrorReady = true
-				if (this.refreshCodemirror) {
-					this.refreshCM()
-				}
-			},
 			onCmCodeChange (newCode) {
 				// console.log('changed')
 				this.code = newCode
-			}
+			},
+			refresh: ViewXmlProEditorFunctions.refresh,
+			refreshCM: ViewXmlProEditorFunctions.refreshCM,
+			onCmReady: ViewXmlProEditorFunctions.onCmReady,
 		}
 	}
 </script>
