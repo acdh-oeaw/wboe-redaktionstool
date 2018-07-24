6d4b615909f48310ed3182a179ee8f0d02da6ecf
hcb79 <hcb@hcb.de>
2018-07-24 11:07:39 +0200

    Neuer Parser ... parseXmlObject - Kinder verarbeiten und Ergebniss weiter geben.

--- src/renderer/components/ToolPage2/functionParser.js
+++ src/renderer/components/ToolPage2/functionParser.js
@@ -8,10 +8,8 @@ const localFunctions = {
 			alert('Fehler! "xmlObject" konnte nicht ausgelesen werden!')
 			return undefined
 		}
-		xmlObject = xmlObject.c
-		match.content = parseIt(xmlObject, match.content)
-		// console.log(match)
-		return match
+		xmlObject = parseIt(xmlObject.c, match.content)
+		return xmlObject
 	}
 }
 
@@ -19,17 +17,34 @@ export default localFunctions
 
 function parseIt (xmlObject, parser) {
 	xmlObject.forEach(function (obj, pos) {
-		console.log(compareIt(obj, pos, parser, xmlObject))
+		let aCompare = compareIt(obj, pos, parser, xmlObject)
+		if (aCompare.errors.length > 0) {
+			obj.error = aCompare.errors
+		}
+		if (aCompare.parserKey > -1) {
+			console.log(obj.n, 'compareIt', aCompare)
+			obj.parser = parser[aCompare.parserKey]
+			if (Array.isArray(obj.c) && obj.c.length > 0) {
+				obj.c = parseIt(obj.c, obj.parser)
+			}
+		} else {
+			console.log(obj.n, '#unknowen')
+			obj.parser = { 'n': '#unknowen', 'p': { 'options': { 'title': { 'value': 'Unbekannt', 'use': true } } } }
+		}
 	})
-	return parser
+	return xmlObject
 }
 
 function compareIt (obj, pos, parser, siblings) {
-	var xobj = {}
+	var errors = []
 	// Berechnen wie hoch die Übereinstimmung ist
 	var pMatch = []
+	if (!Array.isArray(parser) || parser.length === 0) {
+		errors.push({'e': 'Kein parser übergeben!'})
+		return {'parserKey': -1, 'errors': errors}
+	}
 	parser.some(function (par, pPos) {
-		pMatch[pPos] = {'score': 0, 'errors': []}
+		pMatch[pPos] = {'key': pPos, 'score': 0, 'errors': []}
 		if (par.n === obj.n) {		// Namen überprüfen
 			pMatch[pPos].score += 1
 			if (pPos === pos) {		// Position überprüfen
@@ -42,20 +57,32 @@ function compareIt (obj, pos, parser, siblings) {
 			if (attrMatchErrors.length === 0) {
 				pMatch[pPos].score += 1
 			} else {
-				pMatch[pPos].errors.push({'t': 'attributes', 'e': attrMatchErrors})
+				pMatch[pPos].errors.push({'t': 'attributes', 'se': attrMatchErrors})
 			}
 			let valMatchErrors = checkValue(obj, par.p.options.value)		//	Value überprüfen
 			if (valMatchErrors.length === 0) {
 				pMatch[pPos].score += 1
 			} else {
-				pMatch[pPos].errors.push({'t': 'value', 'e': valMatchErrors})
+				pMatch[pPos].errors.push({'t': 'value', 'se': valMatchErrors})
 			}
+		} else {
+			pMatch[pPos].errors.push({'t': 'tag', 'e': 'Keine Übereinstimmung gefunden!'})
 		}
 	})
 	// ToDo: pMatch sortieren nach Fehlern und "score"
-	console.log(pMatch)
-
-	return xobj
+	// Fehler auswerten:
+	if (pMatch[0].errors.length > 0) {
+		errors.push({'e': 'Enthält Fehler!', 'se': pMatch[0].errors})
+	}
+	if (pMatch.length > 1) {
+		if (pMatch[0].score === pMatch[1].score) {
+			errors.push({'e': 'Zuordnung nicht eindeutig!'})
+		}
+	}
+	if (pMatch[0].score === 0) {
+		errors.push({'e': 'Keine Übereinstimmung gefunden!'})
+	}
+	return {'parserKey': pMatch[0].key, 'errors': errors}
 }
 
 function checkValue (objValue, parValue) {
@@ -65,7 +92,7 @@ function checkValue (objValue, parValue) {
 		errors.push({'e': 'Es sollte kein Wert vorhanden sein!'})
 	} else if (parValue !== undefined) {
 		let aVal = objValue.v || ''
-		// ToDo: innerText ... usw.
+		// ToDo: innerText, min, max ... usw.
 		if (aVal === undefined && !parValue.canBeEmpty) {
 			errors.push({'e': 'Wert darf nicht leer sein!'})
 		}
