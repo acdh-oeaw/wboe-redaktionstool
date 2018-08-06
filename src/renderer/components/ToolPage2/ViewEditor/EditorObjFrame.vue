7acd08a27115eedf56481f0c9be66bfd6558efd9
hcb79 <hcb@hcb.de>
2018-08-06 15:37:36 +0200

    "Editor" -> Vorbereitung Kontextmenü.

--- src/renderer/components/ToolPage2/ViewEditor/EditorObjFrame.vue
+++ src/renderer/components/ToolPage2/ViewEditor/EditorObjFrame.vue
@@ -1,32 +1,47 @@
 <template>
-	<b-card class="obj paneldecent mib10" v-if="layoutBase === 'panel'"
-					:header="title" no-body>
-		<div slot="header">
-			<button v-b-toggle="'collapse-' + _uid" class="header-btn-toggle">
-				<span><b>{{ title }}</b></span>
-				<font-awesome-icon :icon="((isOpen) ? 'eye' : 'eye-slash')" class="float-right fa-icon"/>
-			</button>
-		</div>
-		<b-collapse v-model="isOpen" :id="'collapse-' + _uid">
-			<b-card-body>
-				<slot/>
-				<slot name="childs"/>
-			</b-card-body>
-		</b-collapse>
-	</b-card>
-
-	<div class="obj just-childs" v-else-if="layoutBase === 'justChilds'">
+	<div class="obj just-childs" v-if="layoutBase === 'justChilds'">
 		<slot name="childs"/>
 	</div>
 
-	<div :class="'obj lb-' + layoutBase" v-else>
-		<b v-if="title">{{ title }}:</b><br v-if="layoutBase === 'box'"/>
-		<slot/>
-		<slot name="childs"/>
+	<div class="inline" v-else>
+		<b-card class="obj paneldecent mib10" v-if="layoutBase === 'panel'"
+						:header="title" no-body>
+			<div @contextmenu.prevent="contextMenue" slot="header">
+				<button v-b-toggle="'collapse-' + _uid" class="header-btn-toggle">
+					<span><b>{{ title }}</b></span>
+					<font-awesome-icon :icon="((isOpen) ? 'eye' : 'eye-slash')" class="float-right fa-icon"/>
+				</button>
+			</div>
+			<b-collapse v-model="isOpen" :id="'collapse-' + _uid">
+				<b-card-body>
+					<div @contextmenu.prevent="contextMenue" class="context">
+						<slot/>
+					</div>
+					<slot name="childs"/>
+				</b-card-body>
+			</b-collapse>
+		</b-card>
+
+		<div :class="'obj lb-' + layoutBase" v-else>
+			<div @contextmenu.prevent="contextMenue" class="context">
+				<b v-if="title">{{ title }}:</b><br v-if="layoutBase === 'box'"/>
+				<slot/>
+			</div>
+			<slot name="childs"/>
+		</div>
+
+		<vue-context ref="contextMenuEditor" v-if="contextMenuCached">
+			<div class="context-menu-editor-title"><b>Tag:</b> {{ this.content.orgXmlObj.name }}</div>
+			<ul>
+				<li>test ...</li>
+			</ul>
+		</vue-context>
 	</div>
 </template>
 
 <script>
+	import { VueContext } from 'vue-context'
+
 	export default {
 		name: 'EditorObjFrame',
 		props: {
@@ -35,6 +50,7 @@
 		data () {
 			return {
 				'isOpen': true,
+				contextMenuCached: false,
 			}
 		},
 		computed: {
@@ -58,16 +74,36 @@
 			}
 		},
 		methods: {
+			contextMenue: function (e) {
+				this.contextMenuCached = true
+				this.$nextTick(() => { this.$refs.contextMenuEditor.open(e) })
+			}
 		},
+		components: {
+			VueContext
+		}
 	}
 </script>
 
 <style scoped>
+	.inline, .context {
+		display: inline;
+		cursor: default;
+	}
+
 	.obj.lb-inline {
 		display: inline-block;
 		padding: 0px 3px;
 	}
 	.obj.lb-inline:hover {
 		background: #eee;
+	}
+
+	.context-menu-editor-title {
+		padding: 2px 10px;
+		background: #eee;
+	}
+	.v-context ul {
+		padding: 0 !important;
 	}
 </style>
