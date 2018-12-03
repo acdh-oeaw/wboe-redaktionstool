589d439fa07b64bf7da8cfe8b510694a8c56512c
hcb <hcb@hcb.de>
2018-12-03 12:10:20 +0100

    "XRlvModal.vue" -> Dateien auflisten und auswählen.

--- src/renderer/components/ToolPage/ViewEditor/fxFunctions/XRlvModal.vue
+++ src/renderer/components/ToolPage/ViewEditor/fxFunctions/XRlvModal.vue
@@ -1,18 +1,45 @@
 <template>
 	<span :id="'xrlv' + content.uId" class="xrlvmodal">
-		<button @click="edit=true" class="btn-none xrlvmodalbtn view">
+		<button @click="edit = true" class="btn-none xrlvmodalbtn view">
 			view
 			<font-awesome-icon icon="external-link-alt"/>
 		</button>
-		<b-modal v-if="edit" ref="editmodal" :id="'xrlvmodal' + content.uId" title="Querverweis auf Artikel" @hidden="edit=false" @ok="saveValue" @hide="chancelValue">
-			<p>xxxx</p>
+		<b-modal v-if="edit" ref="editmodal" :id="'xrlvmodal' + content.uId" title="Querverweis auf Artikel" @hidden="edit = false" @hide="chancelValue" size="lg">
+			<div class="row">
+				<div class="col-6">
+					<div class="card fileselect">
+  					<div class="card-body">
+							<button @click="selFile = ''" :class="'btn btn-sm w-100 mb-1 text-left btn-' + ((!selFile) ? 'primary' : 'secondary')">{{ cFile }} (Diese Datei)</button>
+							<button @click="selFile = aFile.file" :class="'btn btn-sm w-100 mb-1 text-left btn-' + ((selFile === aFile.file) ? 'primary' : 'secondary')" :key="'fl' + aKey" v-for="(aFile, aKey) in filelist">
+								{{ aFile.file }}
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="card anchorselect">
+  					<div class="card-body">
+							xxx
+						</div>
+					</div>
+				</div>
+			</div>
+			<div slot="modal-footer" class="w-100">
+         <p class="my-2 float-left">{{ selFile }}</p>
+				 <button type="button" @click="saveValue(); $refs.editmodal.hide();" class="btn btn-primary float-right ml-2">OK</button>
+				 <button type="button" @click="$refs.editmodal.hide()" class="btn btn-secondary float-right">Cancel</button>
+			</div>
 		</b-modal>
 	</span>
 </template>
 
 <script>
+	import { remote } from 'electron'
+	import { mapState } from 'vuex'
 	// import _ from 'lodash'
 	// import stdFunctions from '@/functions/stdFunctions'
+	import fPath from 'path'
+	const fs = remote.require('fs')
 
 	export default {
 		name: 'XRlvModal',
@@ -23,13 +50,23 @@
 			return {
 				'edit': false,
 				'changed': false,
+				'filelist': [],
+				'cFile': '',
+				'selFile': '',
 			}
 		},
 		computed: {
+			...mapState(['Parser']),
+			...mapState(['Files']),
 		},
 		watch: {
+			'selFile' () {
+				// ToDo ...
+			},
 			'edit' (nVal) {
 				if (nVal) {
+					this.getBaseData()
+					this.updateFileList()
 					this.$nextTick(() => {
 						this.$refs.editmodal.show()
 					})
@@ -44,6 +81,7 @@
 			},
 		},
 		mounted () {
+			this.cFile = this.Files.file.substr(this.Files.file.length - this.Files.file.split('\\').pop().split('/').pop().length)
 			console.log('XRlvModal', this.content)
 		},
 		methods: {
@@ -56,6 +94,24 @@
 					this.changed = false
 				} else {
 					e.preventDefault()
+				}
+			},
+			getBaseData () {
+				this.selFile = ''
+			},
+			updateFileList () {
+				this.filelist = []
+				if (this.Files.file) {
+					let aPath = this.Files.file.substr(0, this.Files.file.length - this.Files.file.split('\\').pop().split('/').pop().length)
+					fs.readdirSync(aPath).forEach(function (file) {
+						let aFullFileName = fPath.join(aPath, file)
+						if (!fs.statSync(aFullFileName).isDirectory()) {
+							let aExt = file.split('.').pop()
+							if (aExt === 'xml' && file.substr(0, 6) !== 'parser' && file !== this.cFile) {
+								this.filelist.push({ 'file': file, 'fullFileName': aFullFileName, 'title': null, 'anchors': [], 'show': true, 'errors': 0, 'warnings': 0, 'changed': false })
+							}
+						}
+					}, this)
 				}
 			},
 		},
@@ -74,5 +130,10 @@
 	}
 	.xrlvmodal > div, .xrlvmodal > div > div {
 		display:inline-block;
+	}
+	.fileselect, .anchorselect {
+		height: calc( 100vh - 240px );
+		min-height: 300px;
+		overflow-y: auto;
 	}
 </style>
